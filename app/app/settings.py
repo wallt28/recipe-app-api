6eaa812d2bcadb7af7b26bfcbe4d8f245f272654
Tom Wallis <tomwallis@Toms-MacBook-Pro.local>
2022-12-07 15:04:10 +0000

    updated

--- app/app/settings.py
+++ app/app/settings.py
@@ -127,3 +127,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
+# Setting Auth_User_Model configuration
+# Out of all the models defined in our core app, choose the 'User' model
